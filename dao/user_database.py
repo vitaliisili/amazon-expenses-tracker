7e9e24a9803cfe94e383bb0b30ac0df0d9d476a7
Vitalii Sili <vitaliisili@yahoo.com>
2023-04-01 22:49:12 +0200

    change to default value all items in user_db

--- dao/user_database.py
+++ dao/user_database.py
@@ -1,20 +1,23 @@
+from config.app_config import AMAZON_SPENDING_LIMIT
+
 """
 Imitation for database dictionary is used to save data from user input,
-<user_db> contains user base information and user order list
-use <user_db> to do CRUD operations
+<user_db> contains user base information and user order list.
+Use <user_db> to do CRUD operations
 """
 
 user_db = {
-    "username": str,
-    "password": str,
-    "phone": str,
+    "username": "",
+    "password": "",
+    "phone": "",
+    "spending_limit": AMAZON_SPENDING_LIMIT,
     "orders": [
         {
-            "purchase_date": str,
-            "item_name": str,
-            "total_cost": float,
-            "weight": float,
-            "quantity": int
+            "purchase_date": "",
+            "item_name": "",
+            "total_cost": 0.0,
+            "weight": 0.0,
+            "quantity": 0
         }
     ]
 }
