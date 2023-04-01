5b2b211ee97230dc5dc6d2cc12b06d436b39aa9a
Vitalii Sili <vitaliisili@yahoo.com>
2023-04-01 22:51:52 +0200

    add empty function for order validation

--- service/validation_service.py
+++ service/validation_service.py
@@ -23,3 +23,48 @@ def is_phone_valid(phone_number: str) -> bool:
     :return:
     """
     return True  # TODO: add logic for German phone number validation return boolean
+
+
+def is_date_valid(date: str) -> bool:
+    """  TODO: add base description
+    TODO: add full description and parameters description
+    :param date:
+    :return:
+    """
+    return True  # TODO add logic to check if date is valid and format is MM/DD/YYYY  or MM-DD-YYYY
+
+
+def is_item_name_valid(name: str) -> bool:
+    """  TODO: add base description
+    TODO: add full description and parameters description
+    :param name:
+    :return:
+    """
+    return True  # TODO: add logic to check if item name is valid
+
+
+def is_item_cost_valid(cost: str) -> bool:
+    """  TODO: add base description
+    TODO: add full description and parameters description
+    :param cost:
+    :return:
+    """
+    return True  # TODO: add logic if cost is present and can be cast to float
+
+
+def is_item_weight_valid(weight: str) -> bool:
+    """  TODO: add base description
+    TODO: add full description and parameters description
+    :param weight:
+    :return:
+    """
+    return True  # TODO: add logic if weight is present and can be cast to float
+
+
+def is_item_quantity_valid(quantity: str) -> bool:
+    """ TODO: add base description
+    TODO: add full description and parameters description
+    :param quantity:
+    :return:
+    """
+    return True  # TODO: add logic if quantity is present and can be cast to int
