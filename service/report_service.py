d57d608a7379a88bbd2f3bb2f03931ff0b4a4e61
Vitalii Sili <vitaliisili@yahoo.com>
2023-04-01 22:46:14 +0200

    add report service module

---
+++ service/report_service.py
@@ -0,0 +1,54 @@
+def generate_report() -> None:
+    """  TODO: add base description
+    TODO: add full description
+    :return:
+    """
+    pass
+
+
+def get_delivery_cost(weight: float, kg_cost: int) -> float:
+    """  TODO: add base description
+    TODO: add full description
+    :param weight:
+    :param kg_cost:
+    :return:
+    """
+    pass  # TODO: add logic to calculate delivery and return float
+
+
+def get_item_cost(item: dict) -> float:
+    """  TODO: add base description
+    TODO: add full description
+    :param item:
+    :return:
+    """
+    pass  # TODO add logic that calculate cost only for one item and return it
+
+
+def total_delivery_charges(orders: list) -> float:
+    """  TODO: add base description
+    TODO: add full description
+    :param orders:
+    :return:
+    """
+    pass  # TODO: add logic to calculate total delivery for all orders and return it
+
+
+def total_items_cost(orders: list) -> float:
+    """  TODO: add base description
+    TODO: add full description
+    :param orders:
+    :return:
+    """
+    pass  # TODO: add logic to calculate total cost for all items and return it
+
+
+def calculate_average(orders: list) -> float:
+    """  TODO: add base description
+    TODO: add full description
+    :param orders:
+    :return:
+    """
+    pass  # TODO: calculate average for all items
+
+
