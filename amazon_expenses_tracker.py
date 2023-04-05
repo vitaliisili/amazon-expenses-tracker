f93d66a4824ebf242cef8e77e1e4a347d56eac6d
Vitalii Sili <vitaliisili@yahoo.com>
2023-04-05 09:28:04 +0200

    add type for variables

--- amazon_expenses_tracker.py
+++ amazon_expenses_tracker.py
@@ -6,12 +6,12 @@ import config.app_config as config
 
 def main():
     parser = ArgumentParser(description=config.APP_DESCRIPTION, formatter_class=RawTextHelpFormatter)
-    parser.add_argument("username", help="User username")
+    parser.add_argument("username", help="User Username")
     parser.add_argument("password", help="User Password")
 
     args = parser.parse_args()
-    username = args.username
-    password = args.password
+    username: str = args.username
+    password: str = args.password
 
     service.register_user(username, password)
     service.print_message(config.AMAZON_WELCOME_MESSAGE)
